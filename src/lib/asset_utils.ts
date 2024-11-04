5352d4459b13fccfbb99c46281f311d155c195e1
Rémi Even <even.remi@gmail.com>
2024-11-04 12:13:04 +0000

    hopefully fix pic paths, fourth try

--- src/lib/asset_utils.ts
+++ src/lib/asset_utils.ts
@@ -7,14 +7,18 @@ const frameBgs = import.meta.glob('/src/assets/frame_bgs/*.png', { eager: true }
 const frames = import.meta.glob('/src/assets/frames/*.png', { eager: true });
 
 export function getBgPictureLink(gameSlug: string) : string {
-	return import.meta.env.BASE_URL + "frame_bgs/" + gameSlug + ".png";
+	return normalize(import.meta.env.BASE_URL + "/frame_bgs/" + gameSlug + ".png");
 }
 
 export function getFramePictureLink(outfitSlug: string) : string {
-	return import.meta.env.BASE_URL + "frames/" + toFrameType(outfitSlug) + ".png";
+	return normalize(import.meta.env.BASE_URL + "/frames/" + toFrameType(outfitSlug) + ".png");
 }
 
 export function getFacePicLink(characterSlug: string, outfitSlug: string) : string {
 	const outfitSuffix = outfitSlug === "base" ? '' : '_' + outfitSlug;
-	return import.meta.env.BASE_URL + "characters/" + characterSlug + outfitSuffix + "_face.webp";
+	return normalize(import.meta.env.BASE_URL + "/characters/" + characterSlug + outfitSuffix + "_face.webp");
+}
+
+function normalize(url: string): string {
+	return url.replace('//', '/')
 }

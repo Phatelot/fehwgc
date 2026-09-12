a5ae7078283767bdd1843421d4fbec45ead8c228
Phatelot <phatelot@outlook.fr>
2026-09-12 17:26:33 +0200

    add new characters

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -234,6 +234,17 @@ const threeHousesProfessionalsGroup = {
     slug: 'professionals',
 }
 
+const fortunesWeaveGamePrecursor = {
+    name: "Fortune's Weave",
+    nameSlug: "fortunes_weave",
+    darkColor: "",
+    lightColor: "",
+}
+const fortunesWeaveDagdanGroup = {
+    name: "Dagdan",
+    slug: "dagdan",
+}
+
 export const baseMetadata: GameBaseMetadata[] = [
     {
         ...awakeningGamePrecursor,
@@ -1359,6 +1370,14 @@ export const baseMetadata: GameBaseMetadata[] = [
                         outfitWeightThresholdInLb: 350,
                         introducedAfterDonation: 1046,
                     },
+                    {
+                        "outfitWeightThresholdInLb": 500.0,
+                        "mainShape": "⌛",
+                        secondaryShape: "⌛",
+                        "outfit": "Legendary replendent",
+                        "outfitSlug": "legendary_resplendent",
+                        introducedAfterDonation: 1409
+                    }
                 ]
             },
             {
@@ -6565,6 +6584,13 @@ export const baseMetadata: GameBaseMetadata[] = [
                         "outfitSlug": "valentine",
                         introducedAfterDonation: 473,
                     },
+                    {
+                        "outfitWeightThresholdInLb": 500.0,
+                        "mainShape": "🍐",
+                        "outfit": "Legendary",
+                        "outfitSlug": "legendary",
+                        introducedAfterDonation: 1409
+                    }
                 ]
             },
             {
@@ -7171,5 +7197,44 @@ export const baseMetadata: GameBaseMetadata[] = [
                 ]
             }
         ],
+    },
+    {
+        ...fortunesWeaveGamePrecursor,
+        characters: [
+            {
+                "name": "Theodora",
+                "nameSlug": "theodora",
+                "heightInCm": 177.0,
+                "build": 'Regular',
+                "group": fortunesWeaveDagdanGroup,
+                "outfits": [
+                    {
+                        "outfitWeightThresholdInLb": 650.0,
+                        "mainShape": "🍎",
+                        secondaryShape: "⌛",
+                        "outfit": "Base",
+                        "outfitSlug": "base",
+                        introducedAfterDonation: 1409
+                    }
+                ]
+            },
+            {
+                "name": "Leda",
+                "nameSlug": "leda",
+                "heightInCm": 167.0,
+                "build": 'Regular',
+                "group": fortunesWeaveDagdanGroup,
+                "outfits": [
+                    {
+                        "outfitWeightThresholdInLb": 350.0,
+                        "mainShape": "💎",
+                        secondaryShape: "🍎",
+                        "outfit": "Base",
+                        "outfitSlug": "base",
+                        introducedAfterDonation: 1409
+                    }
+                ]
+            },
+        ]
     },
 ];
